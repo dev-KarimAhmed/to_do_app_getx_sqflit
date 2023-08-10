fb4a9e0311701bc1200e3e6a9184478efe001fc2
dev-KarimAhmed <dev-KarimAhmed@gmail.com>
2023-08-10 16:36:20 +0300

    switcth theme with GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:to_do_app_getx_sqflit/services/theme_services.dart';
 import 'package:to_do_app_getx_sqflit/ui/pages/notification_screen.dart';
 import 'package:to_do_app_getx_sqflit/ui/theme.dart';
 
@@ -17,10 +18,10 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       theme: Themes.light,
       darkTheme: Themes.dark,
-      themeMode: ThemeMode.light,
+      themeMode: ThemeServices().theme,
       title: 'To Do',
       debugShowCheckedModeBanner: false,
-      home: NotificationScreen(pyload: 'Title | Description | 10:22 Am'),
+      home: HomePage(),
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:to_do_app_getx_sqflit/services/theme_services.dart';
+
+import 'notification_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +15,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            ThemeServices().switchMode();
+            Get.to(NotificationScreen(pyload: 'Title | Description | 10:22 Am'));
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+        centerTitle: true,
+        backgroundColor: context.theme.primaryColor,
+      ),
       body: Container(),
     );
   }
